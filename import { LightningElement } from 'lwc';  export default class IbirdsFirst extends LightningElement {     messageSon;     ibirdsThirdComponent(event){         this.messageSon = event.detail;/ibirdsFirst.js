6c8a6a2913c890570d01034f438f95027dbd2e3d
Abdul Pathan <101403684+Sherani-Pathan@users.noreply.github.com>
2022-08-08 16:29:14 +0530

    Delete import { LightningElement } from 'lwc';  export default class IbirdsFirst extends LightningElement {     messageSon;     ibirdsThirdComponent(event){         this.messageSon = event.detail; directory

--- import { LightningElement } from 'lwc';  export default class IbirdsFirst extends LightningElement {     messageSon;     ibirdsThirdComponent(event){         this.messageSon = event.detail;/ibirdsFirst.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class IbirdsFirst extends LightningElement {
-    messageSon;
-    ibirdsThirdComponent(event){
-        this.messageSon = event.detail;
-       // console.log('ibirds first1', this.messageSon);
-    }
-}
